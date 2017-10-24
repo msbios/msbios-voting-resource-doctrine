058a4e4915668bdd616c5668db728456c2753a9e
Judzhin Miles <info@msbios.com>
2017-10-24 16:30:22 +0300

    Lazy Composer Commiter

--- src/Module.php
+++ src/Module.php
@@ -15,7 +15,7 @@ use Zend\Loader\StandardAutoloader;
  */
 class Module implements ModuleInterface // , FormElementProviderInterface
 {
-    const VERSION = '1.0.4';
+    const VERSION = '1.0.5';
 
     /**
      * Returns configuration to merge with application configuration
